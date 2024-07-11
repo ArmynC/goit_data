24f5d65034028fd9d652e70f66927f11cf98b418
ArminC <arminandrey@gmail.com>
2024-07-11 18:33:16 +0300

    Create query_terms.sql

---
+++ query_terms.sql
@@ -0,0 +1,21 @@
+-- international_top_terms
+
+SELECT country_name, term, region_name, refresh_date, rank, score, week
+FROM bigquery-public-data.google_trends.international_top_terms
+WHERE country_name = 'Romania'
+LIMIT 100;
+
+SELECT *
+FROM bigquery-public-data.google_trends.international_top_terms
+LIMIT 100;
+
+-- top_terms
+
+SELECT dma_name, term, refresh_date, rank, score, week
+FROM bigquery-public-data.google_trends.top_terms
+WHERE dma_name = 'Romania'
+LIMIT 100;
+
+SELECT *
+FROM bigquery-public-data.google_trends.top_terms
+LIMIT 100;
